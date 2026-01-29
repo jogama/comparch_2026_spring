0942058bbe75b870034bf66b48e4405385b56e3b
Jonathan García-Mallén <jonathangarciamallen@gmail.com>
2026-01-29 11:13:50 -0500

    [pa0] Augment top-level autograder to test the tar archives

--- pa0/assignment_autograder.py
+++ pa0/assignment_autograder.py
@@ -1,9 +1,39 @@
 #!/usr/bin/python3
 
+import sys
 from collatz import autograder as collatz_autograder
 
 total = 0
 
-total += collatz_autograder.grade_collatz ( path="collatz/", verbose=True )
+
+if len( sys.argv ) > 1:
+
+    import tarfile, os, shutil
+
+    with tarfile.open(sys.argv[1]) as tarball:
+        def is_within_directory(directory, target):
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+
+            return prefix == abs_directory
+
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+
+
+        safe_extract(tarball, "tar_test")
+        total += collatz_autograder.grade_collatz ( path="tar_test/pa0/collatz/", verbose=True )
+
+    shutil.rmtree("tar_test")
+
+else:
+    total += collatz_autograder.grade_collatz ( path="collatz/", verbose=True )
 
 print ("Score on assignment: {} out of 20.".format(total))
